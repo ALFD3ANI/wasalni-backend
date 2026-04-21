27c369436cd53588eb25f8447b25f2d867ec2cea
ALFD3ANI <alanazib149@gmail.com>
2026-04-22 02:58:06 +0300

    hardcode db connection

--- src/main/java/com/wasalni/App.java
+++ src/main/java/com/wasalni/App.java
@@ -1,4 +1,3 @@
- 
 package com.wasalni;
 
 import org.springframework.boot.SpringApplication;
@@ -7,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        System.setProperty("spring.datasource.url", 
+            "jdbc:mysql://mysql.railway.internal:3306/railway?useSSL=false&allowPublicKeyRetrieval=true");
+        System.setProperty("spring.datasource.username", "root");
+        System.setProperty("spring.datasource.password", "ZTJGdVStqWTAeujLfwXDgIDfzDJleKor");
+        System.setProperty("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver");
+        System.setProperty("spring.jpa.hibernate.ddl-auto", "none");
         SpringApplication.run(App.class, args);
     }
 }
