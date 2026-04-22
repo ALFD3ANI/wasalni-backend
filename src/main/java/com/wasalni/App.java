cdef4e57c7c40e12e7d2568f9539c38c4e8cbec9
ALFD3ANI <alanazib149@gmail.com>
2026-04-22 04:28:55 +0300

    fix: remove hardcoded credentials

--- src/main/java/com/wasalni/App.java
+++ src/main/java/com/wasalni/App.java
@@ -6,12 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.setProperty("spring.datasource.url", 
-            "jdbc:mysql://roundhouse.proxy.rlwy.net:43646/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC");
-        System.setProperty("spring.datasource.username", "root");
-        System.setProperty("spring.datasource.password", "ZTJGdVStqWTAeujLfwXDgIDfzDJleKor");
-        System.setProperty("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver");
-        System.setProperty("spring.jpa.hibernate.ddl-auto", "none");
         SpringApplication.run(App.class, args);
     }
 }
