ef6aac66ab2d847736353de7c7cf8919df00e9bd
ALFD3ANI <alanazib149@gmail.com>
2026-04-22 03:36:35 +0300

    restore original password

--- src/main/java/com/wasalni/App.java
+++ src/main/java/com/wasalni/App.java
@@ -9,7 +9,7 @@ public class App {
         System.setProperty("spring.datasource.url", 
             "jdbc:mysql://roundhouse.proxy.rlwy.net:43646/railway?useSSL=false&allowPublicKeyRetrieval=true");
         System.setProperty("spring.datasource.username", "root");
-        System.setProperty("spring.datasource.password", "Wasalni2030");
+        System.setProperty("spring.datasource.password", "ZTJGdVStqWTAeujLfwXDgIDfzDJleKor");
         System.setProperty("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver");
         System.setProperty("spring.jpa.hibernate.ddl-auto", "none");
         SpringApplication.run(App.class, args);
